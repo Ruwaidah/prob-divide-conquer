6c56314db9e3ea4d90c6390776dc8854e8665e36
Ruwaidah Alfakhri <Ruwaidah.riyadh@gmail.com>
2023-07-07 17:26:51 -0500

    solving the count zeros function

--- count-zeroes.js
+++ count-zeroes.js
@@ -1,5 +1,21 @@
-function countZeroes() {
-  
+function countZeroes(arr) {
+  const length = arr.length;
+  let leftSideIndex = 0;
+  let rightSideIndex = arr.length - 1;
+  while (arr[leftSideIndex] !== 0 && leftSideIndex <= rightSideIndex) {
+    midIndex = Math.floor((leftSideIndex + rightSideIndex) / 2);
+    if (arr[midIndex] === 1) {
+      leftSideIndex = midIndex + 1;
+    } else if (arr[midIndex] === 0) {
+      rightSideIndex = midIndex - 1;
+    }
+  }
+  return length - leftSideIndex;
 }
 
-module.exports = countZeroes
+// module.exports = countZeroes
+
+console.log(countZeroes([1, 1, 1, 1, 0, 0])); // 2
+console.log(countZeroes([1, 0, 0, 0, 0])); // 4
+console.log(countZeroes([0, 0, 0])); // 3
+console.log(countZeroes([1, 1, 1, 1])); // 0
